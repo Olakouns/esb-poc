2adc554e8fa0186c35dd29611fee9ac9889a9078
Razacki Kounasso <rkounasso@metalafrique.local>
2024-02-02 09:49:00 +0000

    test DeferredResult

---
+++ global_esb/src/main/java/sn/esmt/gesb/controllers/QueueManagerController.java
@@ -0,0 +1,32 @@
+package sn.esmt.gesb.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.ForkJoinPool;
+
+@RestController
+@RequestMapping("queue")
+public class QueueManagerController {
+
+    @GetMapping("/async-deferredresult")
+    public DeferredResult<ResponseEntity<?>> handleReqDefResult() {
+        System.err.println("Received async-deferredresult request");
+        DeferredResult<ResponseEntity<?>> output = new DeferredResult<>();
+
+        ForkJoinPool.commonPool().submit(() -> {
+            System.err.println("Processing in separate thread");
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+            }
+            output.setResult(ResponseEntity.ok("ok"));
+        });
+
+        System.err.println("servlet thread freed");
+        return output;
+    }
+}
