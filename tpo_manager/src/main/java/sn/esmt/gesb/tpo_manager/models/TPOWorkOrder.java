ad990cf742be82fd7650f103d785c27cf19f79a4
Razacki Kounasso <rkounasso@metalafrique.local>
2024-06-14 15:23:30 +0000

    UPDATE

--- tpo_manager/src/main/java/sn/esmt/gesb/tpo_manager/models/TPOWorkOrder.java
+++ tpo_manager/src/main/java/sn/esmt/gesb/tpo_manager/models/TPOWorkOrder.java
@@ -1,17 +1,12 @@
 package sn.esmt.gesb.tpo_manager.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import sn.esmt.gesb.tpo_manager.models.chain.ListNode;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 
 @Entity
@@ -29,28 +24,7 @@ public class TPOWorkOrder implements Serializable {
     @Column(columnDefinition = "TEXT")
     private String template;
     private String equipment;
-//    @OneToMany(cascade = CascadeType.PERSIST)
-//    @JoinColumn(name = "tpo_work_failure_id")
-//    private List<TPOWorkOrder> tpoWorkOrderFailure = new ArrayList<>();
     private boolean canBeDelete;
     @Transient
     private TpoFailureState tpoFailureState;
-//    @JsonIgnore
-//    @ManyToOne
-//    private ListNode listNode;
-//    @Transient
-//    private List<TPOWorkOrder> linkedList;
-//    public List<TPOWorkOrder> getLinkedList() {
-//        LinkedList<TPOWorkOrder> patternsLink = new LinkedList<>();
-//        ListNode current = listNode;
-//        if (current == null) {
-//            return tpoWorkOrderFailure;
-//        }
-//        do {
-//            ListNode finalCurrent = current;
-//            patternsLink.add(tpoWorkOrderFailure.stream().filter(p -> p.getId() == finalCurrent.getDataId()).findFirst().orElse(null));
-//            current = current.getNextNode();
-//        } while (current != null);
-//        return patternsLink;
-//    }
 }
